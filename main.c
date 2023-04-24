c1a67950f7700bb22f29038a3270f124f2106cda
KarlJosephKumar <kjkumr@gmail.com>
2023-04-24 13:47:05 +0300

    Main, .h

---
+++ main.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kakumar <kakumar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/24 13:21:19 by kakumar           #+#    #+#             */
+/*   Updated: 2023/04/24 13:45:41 by kakumar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void init_shell(void)
+{
+	printf("\n\n\n\n Minishell by jofoto and kakumar \n\n\n\n");
+}
+
+void get_pwd(void)
+{
+	char cwd[1024];
+
+	getcwd(cwd, sizeof(cwd));
+	printf("Current dir: %s\n", cwd);
+}
+
+int	take_input(char *str)
+{
+	char	*buff;
+	int		len;
+	
+	len = ft_strlen(buff);
+	buff = readline("minishell$ ");
+	if (len != 0)
+	{
+		add_history(buff);
+		ft_strlcpy(str, buff, len);
+		return (0);
+	}
+	else
+		return (1);
+}
+
+int main(void)
+{
+	char inputstr[100000];
+	
+	init_shell();
+	while (1)
+	{
+		get_pwd();
+		if (take_input(inputstr))
+			continue;
+	}
+}
---
+++ minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kakumar <kakumar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/24 13:25:28 by kakumar           #+#    #+#             */
+/*   Updated: 2023/04/24 13:46:24 by kakumar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include "libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
